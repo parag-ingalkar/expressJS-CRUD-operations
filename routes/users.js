a9168df4e1533cbdf6f4f77e0f8d857d5a3a1e26
parag-ingalkar <paragingalkar@gmail.com>
2025-05-19 13:53:14 -0400

    Implemented /lastName/:lastName route and /sort route

--- routes/users.js
+++ routes/users.js
@@ -38,7 +38,31 @@ router.get("/:email",(req,res)=>{
   res.send(user)//This line is to be replaced with actual return value
 });
 
+router.get("lastName/:lastName",(req,res)=>{
+    // Copy the code here
+    const lastName = req.params.lastName;
+    let user = users.filter((user) => user.lastName === lastName);
+  
+    res.send(user)//This line is to be replaced with actual return value
+  });
 
+  // Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(yyyy + "/" + mm + "/" + dd);
+}
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    // Sort the users array by DOB in ascending order
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    // Send the sorted_users array as the response to the client
+    res.send(sorted_users);
+});
 // POST request: Create a new user
 router.post("/",(req,res)=>{
   // Copy the code here
